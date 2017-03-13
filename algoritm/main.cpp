3a638669ea7ba78834acd70d98a6b6a67b1ecbbc
PTC\odsptc7 <lozaigor88@gmail.com>
2017-03-13 16:49:43 +0200

    1st search

--- algoritm/main.cpp
+++ algoritm/main.cpp
@@ -2,7 +2,46 @@
 
 using namespace std;
 
-int main()
-{
+
+int search(int arr[], const int& size, const int& key) {
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] == key)
+			return i;
+	}
+
+	return -1;
+}
+
+
+bool test(int arr[], const int& size) {
+	for (int i = 0; i < size; ++i) {
+		if (search(arr, size, i) != i) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+
+int main() {
+	const int size = 50;
+	int arr[size];
+	int key = 10;
+
+	for (int i = 0; i < size; ++i) {
+		arr[i] = i;
+	}
+
+	cout << "array element = " << search(arr, size, key) << endl;
+
+	if (test(arr, size)) {
+		cout << "SUCCESS" << endl;
+	}
+	else {
+		cout << "ERROR" << endl;
+	}
+
+	system("pause");
 	return 0;
 }
