dd1e90c0d0906bd9d27aa25bd24315de8921dafa
PTC\odsptc7 <lozaigor88@gmail.com>
2017-03-13 17:54:45 +0200

    algoritm search and test

--- algoritm/main.cpp
+++ algoritm/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
-int search(int arr[], const int& size, const int& key) {
-	for (int i = 0; i < size; ++i) {
-		if (arr[i] == key)
+int search_0(const vector<int>& v, const int& key) {
+	for (int i = 0; i < v.size; ++i) {
+		if (v[i] == key)
 			return i;
 	}
 
@@ -13,34 +14,81 @@ int search(int arr[], const int& size, const int& key) {
 }
 
 
-bool test(int arr[], const int& size) {
-	for (int i = 0; i < size; ++i) {
-		if (search(arr, size, i) != i) {
-			return false;
-		}
+int search_1(int arr[], const int& size, const int& key) {
+	arr[size] = key;
+
+	int i = 0;
+	while (arr[i] != key) {
+		++i;
+	}
+
+	if (i != size) {
+		return i;
 	}
 	
-	return true;
+
+	return -1;
 }
 
 
+int search_2(vector<int>& v, const int& key) {
+	v.push_back(key);
+
+	int i = 0;
+	while (v[i] != key )
+	{
+		++i;
+	}
+
+	v.pop_back();
+
+	if (i != v.size())
+	{
+		return i;
+	}
+
+	return -1;
+}
+
+ostream& operator<<(ostream& o, const vector<T>& v)
+{
+	o << 
+}
+
+template<class TFunc, class TResult, class TParam1>
+void test(TResult expect, TFunc f, TParam1 param) {
+	auto got = f(param);
+	if (got != expect) {
+		cerr << "failed: " << expect << " != " << got << endl;
+	}
+}
+
+template<class TFunc, class TResult, class TParam1, class TParam2>
+void test(TResult expect, TFunc f, TParam1 p1, TParam2 p2) {
+	auto got = f(p1, p2);
+	if (got != expect) {
+		cerr << "failed: " << expect << " != " << got << endl;
+	}
+}
+
+
+void test_search()
+{
+	typedef vector<int> Array;
+
+	test(8, search_2, Array({0,1,2,3,4,5,6,7}))
+}
+
 int main() {
-	const int size = 50;
-	int arr[size];
+	vector<int> v;
 	int key = 10;
 
-	for (int i = 0; i < size; ++i) {
-		arr[i] = i;
+	for (int i = 0; i < v.size; ++i) {
+		v[i] = i;
 	}
 
 	cout << "array element = " << search(arr, size, key) << endl;
 
-	if (test(arr, size)) {
-		cout << "SUCCESS" << endl;
-	}
-	else {
-		cout << "ERROR" << endl;
-	}
 
 	system("pause");
 	return 0;
