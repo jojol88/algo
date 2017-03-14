afdb28e69c28152e8a356bbe14c48474fd74f493
igor <ILoza@kiev.luxoft.com>
2017-03-14 15:06:05 +0200

    refactoring code, added unit test☺

--- algoritm/main.cpp
+++ algoritm/main.cpp
@@ -4,6 +4,43 @@
 using namespace std;
 
 
+ostream& operator<<(ostream& o, const vector<T>& v) {
+	o << "[";
+	for (auto& x : v)
+	{
+		o << x << ", ";
+	}
+
+	return o << "]"
+}
+
+
+template<class TFunc, class TResult, class TParam1>
+void test(TResult expect, TFunc f, TParam1 p1) {
+	auto got = f(p1);
+	if (got != expect) {
+		cerr << "failed: " << expect << " != " << got << endl;
+	}
+}
+
+
+template<class TFunc, class TResult, class TParam1, class TParam2>
+void test(TResult expect, TFunc f, TParam1 p1, TParam2 p2) {
+	auto got = f(p1, p2);
+	if (got != expect) {
+		cerr << "failed: f(" << p1 << ", " << p2 << ") = " << got << " != " << expect << endl;
+	}
+}
+
+
+void test_search()
+{
+	typedef vector<int> Array;
+
+	test(8, search_2, Array({0,1,2,3,4,5,6,7}), 8)
+}
+
+
 int search_0(const vector<int>& v, const int& key) {
 	for (int i = 0; i < v.size; ++i) {
 		if (v[i] == key)
@@ -14,7 +51,7 @@ int search_0(const vector<int>& v, const int& key) {
 }
 
 
-int search_1(int arr[], const int& size, const int& key) {
+int search_1(int arr[], const size_t size, const int& key) {
 	arr[size] = key;
 
 	int i = 0;
@@ -50,45 +87,10 @@ int search_2(vector<int>& v, const int& key) {
 	return -1;
 }
 
-ostream& operator<<(ostream& o, const vector<T>& v)
+
+int main() 
 {
-	o << 
-}
-
-template<class TFunc, class TResult, class TParam1>
-void test(TResult expect, TFunc f, TParam1 param) {
-	auto got = f(param);
-	if (got != expect) {
-		cerr << "failed: " << expect << " != " << got << endl;
-	}
-}
-
-template<class TFunc, class TResult, class TParam1, class TParam2>
-void test(TResult expect, TFunc f, TParam1 p1, TParam2 p2) {
-	auto got = f(p1, p2);
-	if (got != expect) {
-		cerr << "failed: " << expect << " != " << got << endl;
-	}
-}
-
-
-void test_search()
-{
-	typedef vector<int> Array;
-
-	test(8, search_2, Array({0,1,2,3,4,5,6,7}))
-}
-
-int main() {
-	vector<int> v;
-	int key = 10;
-
-	for (int i = 0; i < v.size; ++i) {
-		v[i] = i;
-	}
-
-	cout << "array element = " << search(arr, size, key) << endl;
-
+	test_search();
 
 	system("pause");
 	return 0;
